6862faaf3e5917c98a5ea9835c1b6052567103ac
messe-fp <junior.firmenmesse.fp@gmail.com>
2022-10-12 12:49:09 +0200

    keyboard navigation shortcuts

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  @HostListener('window:keyup', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+
+    switch (event.key) {
+      case "1": 
+        this.router.navigate(['/group-overview']);
+        break;
+
+      case "2": 
+        this.router.navigate(['/bracket-tree']);
+        break;
+
+      case "3": 
+        this.router.navigate(['/team-overview']);
+        break;
+
+      case "4": 
+        this.router.navigate(['/table-overview']);
+        break;
+
+      default: 
+        break;
+    }
+    
   }
 
 }
