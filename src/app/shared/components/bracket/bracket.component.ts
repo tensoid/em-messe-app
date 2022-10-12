2d249f195e4259e8bea402eb777488d827287fa1
messe-fp <junior.firmenmesse.fp@gmail.com>
2022-10-12 15:49:26 +0200

    k.o. tree "growing" per round + winner of ko match marked green

--- src/app/shared/components/bracket/bracket.component.ts
+++ src/app/shared/components/bracket/bracket.component.ts
@@ -6,13 +6,20 @@ import { KOMatchDescription, MatchState } from '../../services/gamedata.service'
   templateUrl: './bracket.component.html',
   styleUrls: ['./bracket.component.scss']
 })
-export class BracketComponent implements OnInit {
+export class BracketComponent {
 
   constructor() { }
 
   @Input()
   match: KOMatchDescription;
 
-  ngOnInit(): void {
+  get winnerIndex(): number {
+
+    if(this.match.state != 2) return -1;
+
+    let team1Goals = this.match.goals.reduce((sum, goals) => sum + goals[0], 0);
+    let team2Goals = this.match.goals.reduce((sum, goals) => sum + goals[1], 0);
+
+    return team1Goals > team2Goals ? 0 : 1;
   }
 }
