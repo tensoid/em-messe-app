1e4d137bed42570016c033a5b02c51bdfaf69638
messe-fp <junior.firmenmesse.fp@gmail.com>
2022-10-14 13:03:42 +0200

    next and previous games buttons replaced with arrow keys

--- src/app/pages/table-overview/table-overview.component.ts
+++ src/app/pages/table-overview/table-overview.component.ts
@@ -1,18 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { GamedataService} from 'src/app/shared/services/gamedata.service';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { GamedataService } from 'src/app/shared/services/gamedata.service';
 
 @Component({
   selector: 'app-table-overview',
   templateUrl: './table-overview.component.html',
-  styleUrls: ['./table-overview.component.scss']
+  styleUrls: ['./table-overview.component.scss'],
 })
 export class TableOverviewComponent implements OnInit {
-
   constructor(public dataService: GamedataService) {}
 
   ngOnInit(): void {}
 
   dataChanged(_: Event) {
     this.dataService.saveData();
+  }
+
+  @HostListener('window:keyup', ['$event'])
+  keyUpEvent(event: KeyboardEvent) {
+    if (event.key == 'ArrowLeft') {
+      this.dataService.returnToPreviousMatches();
+    } else if (event.key == 'ArrowRight') {
+      this.dataService.startNextMatches();
+    }
   }
 }
