88ed8068393ba72f7df44e8c43127d347a54af39
messe-fp <junior.firmenmesse.fp@gmail.com>
2022-10-19 11:20:37 +0200

    bracket tree styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { fadeAnimation } from './animations'
+import { GamedataService } from './shared/services/gamedata.service';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,9 @@ import { fadeAnimation } from './animations'
   animations: [fadeAnimation]
 })
 export class AppComponent {
+
+  constructor(public dataService: GamedataService){}
+
   title = 'juniorenfirma-messe-app';
 }
 
